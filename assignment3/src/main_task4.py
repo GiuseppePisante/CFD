a9b6ed55d23669506f864af82db47febf3d6421e
martinaraffaelli <martina.raffaelli@mail.polimi.it>
2024-11-17 16:08:08 +0100

    added assignment3 folder

---
+++ assignment3/src/main_task4.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.colors import Normalize
+import time 
+
+# TODO: implement code for task 4
