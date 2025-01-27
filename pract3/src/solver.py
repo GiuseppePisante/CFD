d877b09d3333585f338b0e6f7545ef1ae50ddf16
giuseppepisante <giuseppe.pisante@mail.polimi.it>
2025-01-27 17:45:56 +0100

    changes

--- pract3/src/solver.py
+++ pract3/src/solver.py
@@ -5,12 +5,12 @@ def simple_solver(u,v,p,u_star,v_star,u_new,v_new,p_new,pc,d_n,d_e,N,nu,b,h):
     # Under-relaxation factors
     alpha = 0.65
     alpha_p = 0.35
-    err =1
+    err_u, err_v, err_p =1, 1, 1
     it=0
     itermax=100
     tol = 1e-2
 
-    while ((err > tol) & (it<itermax)):
+    while ((err_u > tol) & (err_v > tol) & (err_p > tol) &(it<itermax)):
 
         for i in range(1, N-1):  # Loop over internal cells
             for j in range(1, N-1):
@@ -21,16 +21,16 @@ def simple_solver(u,v,p,u_star,v_star,u_new,v_new,p_new,pc,d_n,d_e,N,nu,b,h):
                 v_S = 0.5*(v[i,j-1] + v[i,j])
 
                 # Coefficients (Convective and Diffusive)
-                a_C = 0.5 * u_E * h - 0.5 * u_W * h + 0.5 * v_S * h - 0.5 * v_N * h
+                a_C = 0.5 * u_E * h - 0.5 * u_W * h + 0.5 * v_S * h - 0.5 * v_N * h - 2* nu
                 a_N= -0.5 * v_N * h + nu
                 a_S = 0.5 * v_S * h + nu
                 a_E = - 0.5 * u_W * h - nu
                 a_W = 0.5 * u_E * h - nu
-                
-                b= h * (p[i,j+1] - p[i,j])
+
+                d_e[i,j] = -h/a_C
 
                 # Momentum equation (Intermediate u*)
-                u_star[i,j] = (a_E * u[i,j+1] + a_W * u[i,j-1] + a_S * u[i-1,j] + a_N * u[i+1,j] + b) / a_C
+                u_star[i,j] = (a_E * u[i,j+1] + a_W * u[i,j-1] + a_S * u[i-1,j] + a_N * u[i+1,j] + h * (p[i,j+1] - p[i,j])) / a_C
 
         for i in range(1, N-1):  # Loop over internal cells
             for j in range(1, N-1):
@@ -41,16 +41,16 @@ def simple_solver(u,v,p,u_star,v_star,u_new,v_new,p_new,pc,d_n,d_e,N,nu,b,h):
                 v_S = 0.5*(v[i,j-1] + v[i,j])
 
                 # Coefficients (Convective and Diffusive)
-                a_C = 0.5 * v_S * h - 0.5 * v_N * h + 0.5 * u_E * h - 0.5 * u_W * h 
+                a_C = 0.5 * v_S * h - 0.5 * v_N * h + 0.5 * u_E * h - 0.5 * u_W * h - 2* nu
                 a_N= -0.5 * v_N * h + nu
                 a_S = 0.5 * v_S * h + nu
                 a_E= 0.5 * u_E * h - nu
                 a_W = -0.5 * u_W * h - nu
                 
-                b = h * (p[i+1,j] - p[i,j])
+                d_n[i,j] = -h/a_C
 
                 # Momentum equation (Intermediate u*)
-                v_star[i,j]= (a_E * v[i,j+1] + a_W * v[i,j-1] + a_S * v[i-1,j] + a_N * v[i+1,j] + b) / a_C
+                v_star[i,j]= (a_E * v[i,j+1] + a_W * v[i,j-1] + a_S * v[i-1,j] + a_N * v[i+1,j] + h * (p[i+1,j] - p[i,j])) / a_C
 
         # Boundary conditions
         u_star[0, :] = u_star[1,:]/3
@@ -64,16 +64,28 @@ def simple_solver(u,v,p,u_star,v_star,u_new,v_new,p_new,pc,d_n,d_e,N,nu,b,h):
         v_star[0, :] = 0  # Top wall
         v_star[N, :] = 0  # Bottom wall
 
+# FINO A QUA
         # Correction term (Initialization)
         pc[0:N, 0:N] = 0
         
-        # Compute the source term b[i, j] for pressure correction
-        for i in range(1, N-1):
-            for j in range(1, N-1):
-                b[i, j] = -((u_star[i, j] - u_star[i, j-1]) / h + (v_star[i, j] - v_star[i-1, j]) / h)
- 
-        # Interior pressure correction
-        pc = solve_pressure_correction(pc, b, a_E, a_W, a_N, a_S, a_C, alpha_p, p, d_e, d_n, h, max_iter=100, tol=1e-6)
+        #Continuity equation a.k.a. pressure correction - Interior
+        for i in range(1,N):
+            for j in range(1,N):
+                a_E = -d_e[i,j]*h
+                a_W = -d_e[i,j-1]*h
+                a_N = -d_n[i-1,j]*h
+                a_S = -d_n[i,j]*h
+                a_P = a_E + a_W + a_N + a_S
+                b[i,j] = -(u_star[i,j] - u_star[i,j-1])*h + (v_star[i,j] - v_star[i-1,j])*h
+
+                pc[i,j] = (a_E*pc[i,j+1] + a_W*pc[i,j-1] + a_N*pc[i-1,j] + a_S*pc[i+1,j] + b[i,j])/a_P
+        
+        
+        #Correcting the pressure field
+        for i in range( 1,N):
+            for j in range( 1,N) :
+                p_new[i,j] = p[i,j] + alpha_p*pc[i,j]
+
 
         # Pressure field Correction
         p_new[1:N, 1:N] = p[1:N, 1:N] + alpha_p * pc[1:N, 1:N]
@@ -107,8 +119,9 @@ def simple_solver(u,v,p,u_star,v_star,u_new,v_new,p_new,pc,d_n,d_e,N,nu,b,h):
 
         
         # Residual computation
-        err = np.sum(np.abs((u_star[1:N, 1:N] - u_star[1:N, :-1]) / h +
-                    (v_star[1:N, 1:N] - v_star[:-1, 1:N]) / h))
+        err_u = np.abs(u_new - u).max() 
+        err_v = np.abs(v_new - v).max()
+        err_p = np.abs(p_new - p).max()
         u = u_new.copy()
         v = v_new.copy()
         p = p_new.copy()
@@ -145,8 +158,8 @@ def Solver(N,L,RE):
 
     u[0,:]=2
 
-    u_new = np.zeros([N+1,N])
-    v_new = np.zeros([N,N+1])
+    u_new = np.zeros([N,N+1])
+    v_new = np.zeros([N+1,N])
     p_new = np.zeros([N+1,N+1])
     
     u_new[0,:]=2
